46e73cde81b26cd279265a474bfb1a7144568fc4
Carlos <cm4997409@gmail.com>
2025-10-29 13:41:41 +0100

    29/10/25

--- Entorno trabajo/Programación/Pruebas/src/Programa.java
+++ Entorno trabajo/Programación/Pruebas/src/Programa.java
@@ -1,29 +1,52 @@
 import java.util.Scanner;
 
-
 public class Programa {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-public static void main(String[]args){
+        System.out.println("Introduce un número de mes: ");
+        int mes= sc.nextInt();
 
-    Scanner sc = new Scanner(System.in);
+            switch (mes) {
+
+                case 12,1,2:
+
+                    System.out.println("En este mes es inverno");
+
+                    break;
+
+                case 3,4,5:
+
+                    System.out.println("En este mes es primavera");
+
+                    break;
+
+                case 6,7,8:
+
+                    System.out.println("En este mes es verano");
+
+                    break;
+
+                case 9,10,11:
+
+                    System.out.println("En este mes es otoño");
+
+                    break;
+
+                default:
+
+                    System.out.println("Opción no válida. Por favor, intentelo de nuevo.");
+
+                    break;
 
 
-    System.out.println("Introduce la cantidad en euros: ");
+            }
+            sc.close();
 
-    double euro = sc.nextDouble();
-
-
-    double dolar = euro * 1.18 ;
+        }
 
 
-    System.out.println("Tu cantidad en dólares es: " + dolar);
+    }
 
 
-sc.close();
-
-
-}
-
-}
-
-
+    
