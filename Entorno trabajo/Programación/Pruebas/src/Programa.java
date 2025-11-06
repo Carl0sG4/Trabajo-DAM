63860f63651159a836f80fe8fe6ffe85d5c44998
cm4997409-githbub <cm4997409@gmail.com>
2025-11-06 22:37:00 +0100

    .

--- Entorno trabajo/Programación/Pruebas/src/Programa.java
+++ Entorno trabajo/Programación/Pruebas/src/Programa.java
@@ -4,29 +4,24 @@ public class Programa {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        final String contraseña = "ABCD";
-        int i = 0; 
-        boolean correcto = false;
+        System.out.print("Ingrese un número entero: ");
+        int N = sc.nextInt();
+
+        int a = 0, b = 1, i = 2;
+
+        System.out.println("0"); 
+        System.out.println("1");   
 
         do {
-            System.out.print("Introduce la contraseña: ");
-            String intento = sc.nextLine();
+            int c = a + b; 
+            System.out.println(c);
+            a = b;
+            b = c;
+            i++;
+
+        } while (i < N);
 
 
-            if (intento.equals(contraseña)) {
-                System.out.println("Contraseña válida");
-                correcto = true;
-            } else {
-                System.out.println("Contraseña inválida");
-                i++;
-            }
-
-        } while (i<3 && !correcto);
-
-        if (!correcto) {
-            System.out.println("Has superado el número máximo de intentos.");
-        }
-        
         sc.close();
     }
 }
