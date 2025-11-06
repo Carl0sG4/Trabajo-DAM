734b25f554b6f6c5ce5226399c35d570157bc83c
Carlos <cm4997409@gmail.com>
2025-11-06 12:44:51 +0100

    6/11/25

--- Entorno trabajo/Programación/Pruebas/src/Programa.java
+++ Entorno trabajo/Programación/Pruebas/src/Programa.java
@@ -1,32 +1,38 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.lang.ArrayIndexOutOfBoundsException;
+
 
 public class Programa {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        final String contraseña = "ABCD";
-        int i = 0; 
-        boolean correcto = false;
+        int [] enteros = {1, 2, 3, 4, 5};
+        
+        try {
+            System.out.println("Introduce un entero divisor: ");
+            int divisor = sc.nextInt();
 
-        do {
-            System.out.print("Introduce la contraseña: ");
-            String intento = sc.nextLine();
+            System.out.println("Introduce un entero posición: ");
+            int posicion = sc.nextInt();
 
+            int resultado = enteros[posicion] / divisor;
+            System.out.println("El resultado es: " + resultado);
 
-            if (intento.equals(contraseña)) {
-                System.out.println("Contraseña válida");
-                correcto = true;
-            } else {
-                System.out.println("Contraseña inválida");
-                i++;
+        } catch (ArithmeticException | ArrayIndexOutOfBoundsException | InputMismatchException e ) {
+            System.out.println("Error: " + e.getMessage());
+
+            if (e instanceof ArithmeticException) {
+                System.out.println("No se puede dividir por cero.");
+            } else if (e instanceof ArrayIndexOutOfBoundsException) {
+                System.out.println("La posición indicada esta fuera de límites.");
+            } else if (e instanceof InputMismatchException) {
+                System.out.println("Entrada inválida. Por favor, introduce un número entero.");
             }
 
-        } while (i<3 && !correcto);
-
-        if (!correcto) {
-            System.out.println("Has superado el número máximo de intentos.");
+        } finally {
+            sc.close();
         }
-        
-        sc.close();
+
     }
 }
