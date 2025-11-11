1713dbfb89fffe96383023f2cb9429b0b21dfb5f
Carlos <cm4997409@gmail.com>
2025-11-11 14:25:25 +0100

    11/11/25-14:25

--- Entorno trabajo/Programación/Pruebas/src/Programa.java
+++ Entorno trabajo/Programación/Pruebas/src/Programa.java
@@ -1,42 +1,34 @@
-import java.util.Scanner;
-import java.util.InputMismatchException;
-import java.lang.ArrayIndexOutOfBoundsException;
-
-/**
- * @author Carlos Guaraca Nagua
- * @since 3 de noviembre de 2025
- */
-
 public class Programa {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int [] enteros = {1, 2, 3, 4, 5};
-        
-        try {
-            System.out.println("Introduce un entero divisor: ");
-            int divisor = sc.nextInt();
+        int totalIteraciones= recorrerBucle(1, 10, 5, 8);
 
-            System.out.println("Introduce un entero posición: ");
-            int posicion = sc.nextInt();
-
-            int resultado = enteros[posicion] / divisor;
-            System.out.println("El resultado es: " + resultado);
-
-        } catch (ArithmeticException | ArrayIndexOutOfBoundsException | InputMismatchException e ) {
-            System.out.println("Error: " + e.getMessage());
-
-            if (e instanceof ArithmeticException) {
-                System.out.println("No se puede dividir por cero.");
-            } else if (e instanceof ArrayIndexOutOfBoundsException) {
-                System.out.println("La posición indicada esta fuera de límites.");
-            } else if (e instanceof InputMismatchException) {
-                System.out.println("Entrada inválida. Por favor, introduce un número entero.");
-            }
-
-        } finally {
-            sc.close();
-        }
+        System.out.println("Número total de iteraciónes: " + totalIteraciones);
 
     }
+
+    public static int recorrerBucle(int inicio, int terminar, int excepcion, int finalizar) {
+
+        int contador = 0;
+
+        for (int i = inicio; i <= terminar; i++) {
+
+            if (i == excepcion) {
+                continue;
+            }
+
+            if (i == finalizar) {
+                break;
+            }
+
+            System.out.println("Índice actual: " + i);
+
+            contador++;
+
+        }
+
+        return contador;
+
+    }
+
 }
