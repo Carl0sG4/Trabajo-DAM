d4c24cda199b953c8c047371494efb7895efe481
Carlos <cm4997409@gmail.com>
2025-11-18 14:24:23 +0100

    18/11/25-14:24

--- Entorno trabajo/Programación/Pruebas/src/Programa.java
+++ Entorno trabajo/Programación/Pruebas/src/Programa.java
@@ -1,34 +1,36 @@
+import java.util.Scanner;
+
 public class Programa {
-    public static void main(String[] args) {
+    public static int leerEntero() {
+        Scanner sc = new Scanner(System.in);
+        int numero = 0; // Entero que Leeremos desde teclado.
+        boolean fin = false;// Instrucción 1
+        do {
+            try {
+                System.out.println("Introduce un entero: ");// Instrucción 2
+                numero = sc.nextInt();
+                fin = true;// Instrucción 3
+                sc.nextLine();// Instrucción 4
 
-        int totalIteraciones= recorrerBucle(1, 10, 5, 8);
-
-        System.out.println("Número total de iteraciónes: " + totalIteraciones);
-
+            } catch (Exception e) {
+                System.out.println("\nERROR: No has introducido un entero.");// Instrucción 5
+                sc.nextLine();// Instrucción 6
+            }
+        } while (!fin);
+        
+        //sc.close();
+        return numero;// Instrucción 8
     }
 
-    public static int recorrerBucle(int inicio, int terminar, int excepcion, int finalizar) {
-
-        int contador = 0;
-
-        for (int i = inicio; i <= terminar; i++) {
-
-            if (i == excepcion) {
-                continue;
-            }
-
-            if (i == finalizar) {
-                break;
-            }
-
-            System.out.println("Índice actual: " + i);
-
-            contador++;
-
+    public static void main(String[] args) {
+        int suma = 0;
+        int cantidadNumeros = leerEntero(); // Leer el número de iteraciones
+        for (int i = 0; i < cantidadNumeros; i++) {
+            System.out.println("Introduce un número a sumar: ");
+            int numero = leerEntero();
+            suma += numero;
         }
-
-        return contador;
-
+        System.out.println("La suma de los números es: " + suma);
     }
 
 }
